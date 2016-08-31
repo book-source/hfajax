2925beea0df47495e7c39c9cf0d7b78dd34be930
Chaniel <netqyq@gmail.com>
2016-08-31 12:00:18 +0800

    first commit

---
+++ ch05-asynchronous-applications/checkName.php
@@ -0,0 +1,16 @@
+<?php
+
+header('Access-Control-Allow-Origin:*');
+$takenUsernames = array ('bill', 'ted', 'sam');
+
+sleep(1);
+
+if ($_REQUEST['username'] == '') {
+	echo 'denied';
+} else if (in_array( $_REQUEST['username'], $takenUsernames )) {
+	echo 'denied';
+} else {
+	echo 'okay';
+}
+
+?>
